2f37125a29045df11cb7138cdf4f43cfb72c5c04
Kennedy Buchichi Masinde <143694221+Buuchezo@users.noreply.github.com>
2026-09-04 13:46:25 +0200

    first implementations

---
+++ src/test/java/com/buuchezo/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buuchezo.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
